df9d30fe4665962cde3ffdb6ecbf241a77ecf884
umerhasan17 <umerhasan17@gmail.com>
2020-01-23 22:35:17 +0000

    Encode train and test data

--- dl/rnn.py
+++ dl/rnn.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
-from ml.ml_helper_functions import sentence_preprocessing
+from ml.ml_helper_functions import sentence_preprocessing, vectorize
 from collections import defaultdict
+from sklearn import model_selection
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 dataset = pd.read_csv('data/airline-tweets.csv')
 data_column = 'text'
@@ -19,7 +21,9 @@ def eda():
 def main():
     eda()
     sentence_preprocessing(dataset, data_column, processed_data_column)
-    print(dataset[data_column][:5], dataset[processed_data_column][:5])
+    train_x, test_x, train_y, test_y = model_selection.train_test_split(dataset[processed_data_column], dataset[target], test_size=0.3)
+    (train_x_vectorized, test_x_vectorized) = vectorize(TfidfVectorizer(max_features=5000), dataset, processed_data_column, train_x, test_x)
+    # todo pad sequences
 
 
 main()
