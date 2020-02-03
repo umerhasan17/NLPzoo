980b405cc5dbc50afbf1e5c5c436d4352082d375
umerhasan17 <umerhasan17@gmail.com>
2020-02-03 21:14:37 +0000

    Add VanillaRNN class

--- dl/rnn.py
+++ dl/rnn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import sys, os
+import torch.nn as nn
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 from ml.ml_helper_functions import sentence_preprocessing, vectorize
 from collections import defaultdict
@@ -18,7 +19,7 @@ def eda():
     print(dataset[target].value_counts())
 
 
-def main():
+def data_prep():
     eda()
     sentence_preprocessing(dataset, data_column, processed_data_column)
     train_x, test_x, train_y, test_y = model_selection.train_test_split(dataset[processed_data_column], dataset[target], test_size=0.3)
@@ -26,4 +27,26 @@ def main():
     # todo pad sequences
 
 
-main()
+class VanillaRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        # why is there a super call here?
+        super(VanillaRNN, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i20 = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+        
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+n_hidden = 128
+rnn = VanillaRNN(26, n_hidden, 2)
