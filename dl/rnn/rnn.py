3d58f94d2aaaf4c5a53f9987e4082c659162c3d4
umerhasan17 <umerhasan17@gmail.com>
2020-02-05 11:10:41 +0000

    debug vanilla RNN: incorrect tensor dimensions

--- dl/rnn/rnn.py
+++ dl/rnn/rnn.py
@@ -4,7 +4,34 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
+class VanillaRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+        super(VanillaRNN, self).__init__()
+        # define params
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        # define layers
+        self.rnn = nn.RNN(input_size, hidden_size, self.n_layers)
+        self.fc = nn.Linear(hidden_size, output_size)
 
+        # self.inputToHidden = nn.Linear(input_size + hidden_size, hidden_size)
+        # self.inputToOutput = nn.Linear(input_size + hidden_size, output_size)
+        # self.softmax = nn.LogSoftmax(dim=1)
+        
+    def forward(self, input, hidden):
+        # combined = torch.stack((input, hidden), 1)
+        # hidden = self.i2h(combined)
+        # output = self.i2o(combined)
+        # output = self.softmax(output)
+        # batch_size = input.size(0)
+        # hidden = self.init_hidden()
+        output, hidden = self.rnn(input, hidden)
+        output = output.contiguous().view(-1, self.hidden_size)
+        output = self.fc(output)
+        return output, hidden
+
+    def init_hidden(self):
+        hidden = torch.zeros(self.n_layers, self.hidden_size)
 
 
 class GruRNN(nn.Module):
