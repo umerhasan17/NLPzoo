019f00de6842edc0ba0af4cd20704279c9b91f6a
umerhasan17 <umerhasan17@gmail.com>
2020-02-07 20:40:04 +0000

    Add helper functions for processing formatted data

---
+++ dl/seq2seq/chatbot/trim.py
@@ -0,0 +1,101 @@
+import re
+import unicodedata
+
+MAX_LENGTH = 10  # Maximum sentence length to consider
+MIN_COUNT = 3    # Minimum word count threshold for trimming
+
+def load_and_trim_data():
+    stem = "../../../data/cornell-movie-dialogs-corpus/"
+    corpus_name = "cornell-movie-dialogs-corpus"
+    formatted_path = datafile = stem + "formatted_movie_lines.txt"
+    save_dir = stem
+    # Load/Assemble voc and pairs
+    voc, pairs = load_and_prepare_data(corpus_name, datafile, save_dir)
+    # Print some pairs to validate
+    print("\npairs:")
+    for pair in pairs[:10]:
+        print(pair)
+    # Trim voc and pairs
+    pairs = trim_rare_words(voc, pairs, MIN_COUNT)
+
+# Turn a Unicode string to plain ASCII, thanks to
+# http://stackoverflow.com/a/518232/2809427
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+def normalize_string(s):
+    s = unicode_to_ascii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
+
+# Read query/response pairs and return a voc object
+def read_vocs(datafile, corpus_name):
+    print("Reading lines...")
+    # Read the file and split into lines
+    lines = open(datafile, encoding='utf-8').\
+        read().strip().split('\n')
+    # Split every line into pairs and normalize
+    pairs = [[normalize_string(s) for s in l.split('\t')] for l in lines]
+    voc = Vocabulary(corpus_name)
+    return voc, pairs
+
+# Returns True iff both sentences in a pair 'p' are under the MAX_LENGTH threshold
+def filter_pair(p):
+    # Input sequences need to preserve the last word for EOS token
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH
+
+# Filter pairs using filterPair condition
+def filter_pairs(pairs):
+    return [pair for pair in pairs if filter_pair(pair)]
+
+# Using the functions defined above, return a populated voc object and pairs list
+def load_and_prepare_data(corpus_name, datafile, save_dir):
+    print("Start preparing training data ...")
+    voc, pairs = read_vocs(datafile, corpus_name)
+    print("Read {!s} sentence pairs".format(len(pairs)))
+    pairs = filter_pairs(pairs)
+    print("Trimmed to {!s} sentence pairs".format(len(pairs)))
+    print("Counting words...")
+    for pair in pairs:
+        voc.add_sentence(pair[0])
+        voc.add_sentence(pair[1])
+    print("Counted words:", voc.num_words)
+    return voc, pairs
+
+def trim_rare_words(voc, pairs, MIN_COUNT):
+    # Trim words used under the MIN_COUNT from the voc
+    voc.trim(MIN_COUNT)
+    # Filter out pairs with trimmed words
+    keep_pairs = []
+    for pair in pairs:
+        input_sentence = pair[0]
+        output_sentence = pair[1]
+        keep_input = True
+        keep_output = True
+        # Check input sentence
+        for word in input_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_input = False
+                break
+        # Check output sentence
+        for word in output_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_output = False
+                break
+
+        # Only keep pairs that do not contain trimmed word(s) in their input or output sentence
+        if keep_input and keep_output:
+            keep_pairs.append(pair)
+
+    print("Trimmed from {} pairs to {}, {:.4f} of total".format(len(pairs), len(keep_pairs), len(keep_pairs) / len(pairs)))
+    return keep_pairs
+
+
+if __name__ == '__main__':
+    load_and_trim_data()
